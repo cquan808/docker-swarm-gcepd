3398584ff62ea60d213551ea49b777d60cf93ec8
Chris <cquan808@gmail.com>
2018-11-14 12:09:48 -0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -6,10 +6,6 @@ sudo apt-key add gpg
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get -y install docker-ce=18.06.1~ce~3-0~ubuntu
-# Increase Virtual Memory for Elasticsearch
-sudo sysctl -w vm.max_map_count=262144
-sudo -s
-sudo echo 'vm.max_map_count=262144' >> sudo /etc/sysctl.conf
 # docker metrics setup
 echo '{ "metrics-addr" : "0.0.0.0:9323", "experimental" : true }' >> /etc/docker/daemon.json
 sudo service docker restart
@@ -17,4 +13,8 @@ curl localhost:9323/metrics
 # install rexray-gcepd
 docker plugin install --grant-all-permissions rexray/gcepd GCEPD_TAG=rexray
 docker volume ls
+# Increase Virtual Memory for Elasticsearch
+sudo sysctl -w vm.max_map_count=262144
+sudo -s
+sudo echo 'vm.max_map_count=262144' >> sudo /etc/sysctl.conf
 exit
